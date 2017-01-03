88f2362fb08d06686a15182e5ba1619bc604e8e6
Sam Rispaud <samrispaud@gmail.com>
2017-01-03 17:56:24 -0500

    WIP

---
+++ src/containers/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>My Counter</div>
+        <h1>14</h1>
+        <hr></hr>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
+}
