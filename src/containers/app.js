bedcf823f79865aaba641ef29179bd785baaf42f
Sam Rispaud <samrispaud@gmail.com>
2017-01-04 10:13:18 -0500

    WIP2

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { add, substract } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 export default class App extends Component {
   render() {
@@ -13,3 +17,19 @@ export default class App extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  console.log('mapStateToProps', state);
+  // Whatever is returned will show up as props
+  return {
+    textBox: state.textBox
+  };
+}
+
+// Anything returned from this function will end up as props
+function mapDispatchToProps(dispatch) {
+  console.log('mapDispatchToProps', dispatch);
+  return bindActionCreators({ updateTextBox: updateTextBox }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
